ef6210893e889c041aa381704e934635bfdaa824
Bartek <bartekd97@gmail.com>
2019-12-14 23:10:12 +0100

    Basic blank window for logistic

---
+++ View/LogisticsForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class LogisticsForm : Form
+    {
+        public LogisticsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
