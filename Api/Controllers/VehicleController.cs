cddbfa3c4cb75968ead100d8e76653857cc8677a
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2019-12-16 00:59:21 +0100

    Added Vehicle Controller and DTO for it

--- Api/Controllers/VehicleController.cs
+++ Api/Controllers/VehicleController.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using System.Text;
 using Api.Services;
-using Api.Enums;
 using Api.DTOs;
+using Api.Enums;
 using Model.ViewModels;
 
 namespace Api.Controllers
 {
-    class VehicleController
+    public class VehicleController
     {
+        private readonly IVehicleService _vehicleService;
+        public VehicleController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
 
+        public VehiclesDTO GetAllVehicles()
+        {
+            try
+            {
+                var vehiclesList = _vehicleService.GetAllVehicles();
+
+                var result = new VehiclesDTO
+                {
+                    Vehicles = vehiclesList,
+                    Status = CollectionGetStatus.Success
+
+                };
+
+                return result;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                var result = new VehiclesDTO
+                {
+                    Status = CollectionGetStatus.Failure
+                };
+
+                return result;
+            }
+        }
+
+        
     }
 }
+
