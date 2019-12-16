f6df64d03d66c6178b97adb9fab978db254697df
PawelKunka <pawelkun@gmail.com>
2019-12-16 15:56:12 +0100

    QRLabelGenerator suggestion

---
+++ Api/Helpers/QRLabelGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Data.Context;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Drawing.Imaging;
+
+
+namespace Api.Helpers
+{
+    public static class QRLabelGenerator
+    {
+        public static void MakeLabel(string filePath)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("Blablabla", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            using (FileStream fs = File.Create(filePath))
+            {
+                qrCodeImage.Save(fs, ImageFormat.Png);
+            }
+        }
+    }
+}
