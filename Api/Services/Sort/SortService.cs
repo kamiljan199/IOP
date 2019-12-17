d0bca7a54f9cb5ad49076f834358626b35f92667
Piotr <gortyn98@gmail.com>
2019-12-17 12:21:10 +0100

    Optimalized sorting
    Sorting by city then street then home number

--- Api/Services/Sort/SortService.cs
+++ Api/Services/Sort/SortService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Api.Managers;
 using Model.Models;
+using System.Linq;
 
 namespace Api.Services
 {
@@ -34,67 +35,12 @@ namespace Api.Services
         public void SortParcelsByAddress()
         {
             List<Parcel> sortedParcels = new List<Parcel>();
-            List<Parcel> sortedByCity = new List<Parcel>();
-            List<Parcel> sortedByStreet = new List<Parcel>();
-            List<Parcel> sortedByNumber = new List<Parcel>();
-            sortedParcels.Clear();
-            Parcel nextParcel = _parcels[0];
-            int index = 0;
-            int numberOfParcelsInMagazine = _parcels.Count;
-            while (sortedByCity.Count < numberOfParcelsInMagazine)
-            {
-                nextParcel = _parcels[0];
-                for (int i = 0; i < _parcels.Count; i++)
-                {
-                    if (_parcels[i].ReceiverData.PersonalAddress.City.CompareTo(nextParcel.ReceiverData.PersonalAddress.City) < 0)
-                    {
-                        nextParcel = _parcels[i];
-                        index = i;
-                    }
-                }
-                sortedByCity.Add(nextParcel);
-                _parcels.RemoveAt(index);
-                index = 0;
-            }
 
-            nextParcel = sortedByCity[0];
-            while (sortedByStreet.Count < numberOfParcelsInMagazine)
-            {
-                nextParcel = sortedByCity[0];
-                for (int i = 0; i < sortedByCity.Count; i++)
-                {
-                    if (sortedByCity[i].ReceiverData.PersonalAddress.City.CompareTo(nextParcel.ReceiverData.PersonalAddress.City) < 0)
-                    {
-                        nextParcel = sortedByCity[i];
-                        index = i;
-                    }
-                }
-                sortedByStreet.Add(nextParcel);
-                sortedByCity.RemoveAt(index);
-                index = 0;
-            }
-
-            nextParcel = sortedByStreet[0];
-            while (sortedByNumber.Count < numberOfParcelsInMagazine)
-            {
-                nextParcel = sortedByStreet[0];
-                for (int i = 0; i < sortedByStreet.Count; i++)
-                {
-                    if (sortedByStreet[i].ReceiverData.PersonalAddress.City.CompareTo(nextParcel.ReceiverData.PersonalAddress.City) < 0)
-                    {
-                        nextParcel = sortedByStreet[i];
-                        index = i;
-                    }
-                }
-                sortedByNumber.Add(nextParcel);
-                sortedByStreet.RemoveAt(index);
-                index = 0;
-            }
-
-            foreach (var parcel in sortedByNumber)
-            {
-                sortedParcels.Add(parcel);
-            }
+            sortedParcels = _parcels
+                .OrderBy(e => e.ReceiverData.PersonalAddress.City)
+                .ThenBy(e => e.ReceiverData.PersonalAddress.Street)
+                .ThenBy(e => e.ReceiverData.PersonalAddress.HomeNumber)
+                .ToList();
         }
     }
 }
