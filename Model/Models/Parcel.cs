2f7d304d324ed1c13251f37c0b7bd6bbf05dbee7
Ziemniak <potatoosalad@gmail.com>
2020-01-11 17:24:48 +0100

    Added Parcel Type and Size

--- Model/Models/Parcel.cs
+++ Model/Models/Parcel.cs
@@ -8,6 +8,10 @@ namespace Model.Models
         public int StorePlaceId { get; set; }
         public PersonalData SenderData { get; set; }
         public PersonalData ReceiverData { get; set; }
+        public float ParcelWidth { get; set; }
+        public float ParcelHeight { get; set; }
+        public float ParcelLength { get; set; }
+        public float ParcelType { get; set; }
         public int ReferenceId { get; set; }
     }
 }
