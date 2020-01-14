b9ff8f85cddcf9730bb005ad1dae20d9d9b77536
kamiljan199 <56162517+kamiljan199@users.noreply.github.com>
2020-01-14 23:24:33 +0100

    Revert "Getters from database - parcel by warehouse and parcel by status from…"

--- Api/Services/Parcels/IParcelService.cs
+++ Api/Services/Parcels/IParcelService.cs
@@ -11,8 +11,6 @@ namespace Api.Services
     {
         public Parcel GetById(int id);
         public Parcel GetByReferenceId(int id);
-        public Parcel[] GetByStorePlace(StorePlace storePlace);
-        public Parcel[] GetFromStorePlaceByStatus(StorePlace storePlace, ParcelStatus status);
         public void PostParcel(Parcel newParcel);
         public void ChangeParcelPriority(Parcel parcelToChange, int priority);
         public void ChangeParcelStatus(Parcel parcelToChange, ParcelStatus status);
