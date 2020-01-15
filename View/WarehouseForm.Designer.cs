c998a5c5ede959b0dad345ef375bd2cc0cef08c2
GroveW <qswdw3ed3@gmail.com>
2020-01-15 23:01:21 +0100

    Load/unload cargo

--- View/WarehouseForm.Designer.cs
+++ View/WarehouseForm.Designer.cs
@@ -36,13 +36,17 @@
             this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
             this.buttonPost = new System.Windows.Forms.Button();
+            this.chooseVehicleTextbox = new System.Windows.Forms.TextBox();
+            this.chooseVehicleCombobox = new System.Windows.Forms.ComboBox();
+            this.vehicleSpaceTakenRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(568, 31);
+            this.buttonLogout.Location = new System.Drawing.Point(568, 12);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(173, 74);
+            this.buttonLogout.Size = new System.Drawing.Size(173, 37);
             this.buttonLogout.TabIndex = 0;
             this.buttonLogout.Text = "Wyloguj";
             this.buttonLogout.UseVisualStyleBackColor = true;
@@ -50,9 +54,9 @@
             // 
             // buttonSort
             // 
-            this.buttonSort.Location = new System.Drawing.Point(568, 257);
+            this.buttonSort.Location = new System.Drawing.Point(569, 271);
             this.buttonSort.Name = "buttonSort";
-            this.buttonSort.Size = new System.Drawing.Size(172, 123);
+            this.buttonSort.Size = new System.Drawing.Size(172, 38);
             this.buttonSort.TabIndex = 1;
             this.buttonSort.Text = "Sortuj";
             this.buttonSort.UseVisualStyleBackColor = true;
@@ -61,7 +65,7 @@
             // storePlaceTextbox
             // 
             this.storePlaceTextbox.Enabled = false;
-            this.storePlaceTextbox.Location = new System.Drawing.Point(217, 12);
+            this.storePlaceTextbox.Location = new System.Drawing.Point(15, 12);
             this.storePlaceTextbox.Name = "storePlaceTextbox";
             this.storePlaceTextbox.Size = new System.Drawing.Size(320, 31);
             this.storePlaceTextbox.TabIndex = 2;
@@ -75,13 +79,13 @@
             this.columnHeader3});
             this.parcelsListView.FullRowSelect = true;
             this.parcelsListView.HideSelection = false;
-            this.parcelsListView.Location = new System.Drawing.Point(15, 64);
+            this.parcelsListView.Location = new System.Drawing.Point(15, 109);
             this.parcelsListView.Name = "parcelsListView";
-            this.parcelsListView.Size = new System.Drawing.Size(534, 315);
+            this.parcelsListView.Size = new System.Drawing.Size(534, 289);
             this.parcelsListView.TabIndex = 3;
             this.parcelsListView.UseCompatibleStateImageBehavior = false;
             this.parcelsListView.View = System.Windows.Forms.View.Details;
-            this.parcelsListView.SelectedIndexChanged += new System.EventHandler(this.parcelsListView_SelectedIndexChanged);
+            this.parcelsListView.SelectedIndexChanged += new System.EventHandler(this.ParcelListView_ItemSelectionChanged);
             // 
             // columnHeader1
             // 
@@ -100,22 +104,64 @@
             // buttonPost
             // 
             this.buttonPost.Enabled = false;
-            this.buttonPost.Location = new System.Drawing.Point(568, 130);
+            this.buttonPost.Location = new System.Drawing.Point(569, 315);
             this.buttonPost.Name = "buttonPost";
-            this.buttonPost.Size = new System.Drawing.Size(172, 67);
+            this.buttonPost.Size = new System.Drawing.Size(172, 64);
             this.buttonPost.TabIndex = 4;
             this.buttonPost.Text = "Nadaj";
             this.buttonPost.UseVisualStyleBackColor = true;
             this.buttonPost.Click += new System.EventHandler(this.ButtonPost_Click);
             // 
+            // chooseVehicleTextbox
+            // 
+            this.chooseVehicleTextbox.Enabled = false;
+            this.chooseVehicleTextbox.Location = new System.Drawing.Point(15, 53);
+            this.chooseVehicleTextbox.Name = "chooseVehicleTextbox";
+            this.chooseVehicleTextbox.Size = new System.Drawing.Size(139, 31);
+            this.chooseVehicleTextbox.TabIndex = 5;
+            this.chooseVehicleTextbox.Text = "Wybierz pojazd:";
+            // 
+            // chooseVehicleCombobox
+            // 
+            this.chooseVehicleCombobox.FormattingEnabled = true;
+            this.chooseVehicleCombobox.Location = new System.Drawing.Point(160, 51);
+            this.chooseVehicleCombobox.Name = "chooseVehicleCombobox";
+            this.chooseVehicleCombobox.Size = new System.Drawing.Size(229, 33);
+            this.chooseVehicleCombobox.TabIndex = 6;
+            this.chooseVehicleCombobox.SelectedValueChanged += new System.EventHandler(this.ChooseVehicleCombobox_SelectedValueChanged);
+            // 
+            // vehicleSpaceTakenRichTextBox
+            // 
+            this.vehicleSpaceTakenRichTextBox.Enabled = false;
+            this.vehicleSpaceTakenRichTextBox.Location = new System.Drawing.Point(555, 90);
+            this.vehicleSpaceTakenRichTextBox.Name = "vehicleSpaceTakenRichTextBox";
+            this.vehicleSpaceTakenRichTextBox.Size = new System.Drawing.Size(185, 60);
+            this.vehicleSpaceTakenRichTextBox.TabIndex = 8;
+            this.vehicleSpaceTakenRichTextBox.Text = "Obciążenie: 0%\nPojemność: 0%";
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Enabled = false;
+            this.buttonLoad.Location = new System.Drawing.Point(574, 162);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(165, 37);
+            this.buttonLoad.TabIndex = 9;
+            this.buttonLoad.Text = "Załaduj";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
             // WarehouseForm
             // 
             this.ClientSize = new System.Drawing.Size(753, 410);
-            this.Controls.Add(this.buttonPost);
             this.Controls.Add(this.parcelsListView);
-            this.Controls.Add(this.storePlaceTextbox);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.vehicleSpaceTakenRichTextBox);
             this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonPost);
             this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.chooseVehicleCombobox);
+            this.Controls.Add(this.storePlaceTextbox);
+            this.Controls.Add(this.chooseVehicleTextbox);
             this.Name = "WarehouseForm";
             this.Load += new System.EventHandler(this.WarehouseForm_Load);
             this.ResumeLayout(false);
@@ -125,7 +171,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox chooseVehicleTextbox;
         private System.Windows.Forms.Button logoutButton;
         private System.Windows.Forms.Button sortButton;
         private System.Windows.Forms.ListView parcelsListView;
@@ -138,5 +184,8 @@
         private System.Windows.Forms.TextBox storePlaceTextbox;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button buttonPost;
+        private System.Windows.Forms.ComboBox chooseVehicleCombobox;
+        private System.Windows.Forms.RichTextBox vehicleSpaceTakenRichTextBox;
+        private System.Windows.Forms.Button buttonLoad;
     }
 }
--- View/WarehouseForm.cs
+++ View/WarehouseForm.cs
@@ -15,10 +15,18 @@ namespace View
         private readonly EmployeeController _employeeController;
         private readonly SortController _sortController;
 
+        private List<Vehicle> _availableVehicles;
+        private List<ListViewItem> _availableParcels;
+
+        private Dictionary<Vehicle, List<ListViewItem>> vehiclesCargoDictionary;
+
         public WarehouseForm(EmployeeController employeeController, SortController sortController)
         {
             _employeeController = employeeController;
             _sortController = sortController;
+            vehiclesCargoDictionary = new Dictionary<Vehicle, List<ListViewItem>>();
+            _availableVehicles = new List<Vehicle>();
+            _availableParcels = new List<ListViewItem>();
             InitializeComponent();
         }
 
@@ -31,14 +39,18 @@ namespace View
         {
             if (_employeeController.GetLoggedEmployee() != null)
             {
-                textBox1.Text = "Id pracownika: " + _employeeController.GetLoggedEmployee().Id +
+                chooseVehicleTextbox.Text = "Id pracownika: " + _employeeController.GetLoggedEmployee().Id +
                 " Magazyn: " + "controller.getStorePlaceForEmployee(id).Id";
             }
 
             string[] parcelInfo1 = { "1", "abc", "def" };
             string[] parcelInfo2 = { "2", "def", "ghi" };
+
             parcelsListView.Items.Add(new ListViewItem(parcelInfo1));
             parcelsListView.Items.Add(new ListViewItem(parcelInfo2));
+
+            _availableParcels.Add(parcelsListView.Items[0]);
+            _availableParcels.Add(parcelsListView.Items[1]);
 
             //List<Parcel> parcels = parcelController.getParcelsByStoreId(storeId).asList();
 
@@ -48,6 +60,36 @@ namespace View
             //    parcel.ReceiverData.PersonalAddress.Street};
             //    parcelsListView.Items.Add(new ListViewItem(parcelInfo));
             //}
+
+            //load Vehicles to combobox
+
+            //List<Vehicles> vechicles = _vehicleController.getVehiclesByStorePlace(controller.getStorePlaceForEmployee(id).Id);
+
+            _availableVehicles.Clear();
+
+            Vehicle v = new Vehicle();
+            v.Brand = "MAN";
+            v.Model = "TGX";
+            v.Registration = "EPD3G53";
+
+            Vehicle v1 = new Vehicle();
+            v1.Brand = "MAN";
+            v1.Model = "TGL";
+            v1.Registration = "EPD3G54";
+
+            _availableVehicles.Add(v);
+            _availableVehicles.Add(v1);
+
+            foreach (var vehicle in _availableVehicles)
+            {
+                string vehicleInfo = vehicle.Brand + " " + vehicle.Model;
+                chooseVehicleCombobox.Items.Add(vehicleInfo);
+            }
+
+            if (_availableVehicles.Count != 0)
+            {
+                //chooseVehicleCombobox.SelectedItem = chooseVehicleCombobox.Items[0];
+            }
         }
 
         private void ButtonSort_Click(object sender, EventArgs e)
@@ -91,15 +133,62 @@ namespace View
             foreach (var parcel in sortedParcels)
             {
                 string[] parcelInfo = { parcel.Id.ToString(),
-                    parcel.SenderData.PersonalAddress.Street,
-                parcel.ReceiverData.PersonalAddress.Street};
+                    parcel.SenderData.PersonalAddress.Street +", "+
+                    parcel.SenderData.PersonalAddress.ApartmentNumber+"/"+
+                    parcel.SenderData.PersonalAddress.HomeNumber + ", "+
+                    parcel.SenderData.PersonalAddress.PostCode+" "+
+                    parcel.SenderData.PersonalAddress.City,
+                parcel.ReceiverData.PersonalAddress.Street +", "+
+                    parcel.ReceiverData.PersonalAddress.ApartmentNumber+"/"+
+                    parcel.ReceiverData.PersonalAddress.HomeNumber + ", "+
+                    parcel.ReceiverData.PersonalAddress.PostCode+" "+
+                    parcel.ReceiverData.PersonalAddress.City};
                 parcelsListView.Items.Add(new ListViewItem(parcelInfo));
+                _availableParcels.Add(new ListViewItem(parcelInfo));
             }
         }
 
         private void ParcelListView_ItemSelectionChanged(object sender, EventArgs e)
         {
             buttonPost.Enabled = parcelsListView.SelectedItems.Count > 0;
+        }
+
+        private void ChooseVehicleCombobox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            buttonLoad.Enabled = chooseVehicleCombobox.SelectedItem != null;
+
+            foreach (ListViewItem parcel in parcelsListView.Items)
+            {
+                parcel.Selected = false;
+
+                if (!_availableParcels.Contains(parcel))
+                {
+                    parcelsListView.Items.Remove(parcel);
+                }
+            }
+
+            if (chooseVehicleCombobox.SelectedItem != null && vehiclesCargoDictionary.ContainsKey(_availableVehicles[chooseVehicleCombobox.SelectedIndex]))
+            {
+                foreach (ListViewItem item in vehiclesCargoDictionary[_availableVehicles[chooseVehicleCombobox.SelectedIndex]])
+                {
+                    parcelsListView.Items.Add(item);
+                    item.Selected = true;
+                }
+            }
+
+            //if (chooseVehicleCombobox.SelectedItem != null && vehiclesCargoDictionary.ContainsKey(_availableVehicles[chooseVehicleCombobox.SelectedIndex]))
+            //{
+            //    foreach(ListViewItem parcel in parcelsListView.Items)
+            //    {
+            //        if (vehiclesCargoDictionary[_availableVehicles[chooseVehicleCombobox.SelectedIndex]].
+            //            Contains(parcel))
+            //        {
+            //            parcel.Selected = true;
+            //        }
+            //    }
+            //}
+
+            parcelsListView.Select();
         }
 
         private void ButtonPost_Click(object sender, EventArgs e)
@@ -113,6 +202,30 @@ namespace View
             {
                 parcelsListView.SelectedItems[0].SubItems[0].Text = "null";
             }
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            Vehicle selectedVehicle = _availableVehicles[chooseVehicleCombobox.SelectedIndex];
+
+            if (vehiclesCargoDictionary.ContainsKey(selectedVehicle))
+            {
+                _availableParcels.AddRange(vehiclesCargoDictionary[selectedVehicle]);
+            }
+
+            vehiclesCargoDictionary.Remove(selectedVehicle);
+
+            List<ListViewItem> items = new List<ListViewItem>();
+
+            foreach(ListViewItem item in parcelsListView.SelectedItems)
+            {
+                items.Add(item);
+                _availableParcels.Remove(item);
+            }
+
+            vehiclesCargoDictionary.Add(selectedVehicle, items);
+
+            parcelsListView.Select();
         }
     }
 }
