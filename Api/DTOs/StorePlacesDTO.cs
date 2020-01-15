1dfb233c415703463ba5c9baa45cb47a1f3adaf5
AandrzejMi <andrzej.mi@op.pl>
2020-01-15 21:35:25 +0100

    StorePlacesDTO

---
+++ Api/DTOs/StorePlacesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.Models;
+using Api.Enums;
+
+namespace Api.DTOs
+{
+    class StorePlacesDTO
+    {
+        public List<StorePlace> StorePlaces { get; set; }
+        public CollectionGetStatus Status;
+    }
+}
