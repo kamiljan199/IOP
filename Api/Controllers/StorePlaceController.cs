cee20ab12ca21028cc2330d250c8a126720cff3f
AandrzejMi <andrzej.mi@op.pl>
2020-01-15 23:08:31 +0100

    StorePlacesManager

---
+++ Api/Controllers/StorePlaceController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Api.DTOs;
+using Api.Services;
+using Api.Enums;
+using Model.Models;
+using System.Linq;
+
+namespace Api.Controllers
+{
+    class StorePlaceController
+    {
+        private IStorePlaceService _storePlaceService;
+
+        public StorePlaceController(IStorePlaceService storePlaceService)
+        {
+            _storePlaceService = storePlaceService;
+        }
+
+        public StorePlacesDTO GetAllStorePlaces()
+        {
+            StorePlacesDTO result;
+            try
+            {
+                var collection = _storePlaceService.GetAll();
+
+                result = new StorePlacesDTO
+                {
+                    StorePlaces = collection,
+                    Status = collection.Count > 0 ? CollectionGetStatus.Success : CollectionGetStatus.Empty
+                };
+            }
+            catch
+            {
+                result = new StorePlacesDTO
+                {
+                    StorePlaces = null,
+                    Status = CollectionGetStatus.Failure
+                };
+            }
+
+            return result;
+        }
+        public StorePlacesDTO GetAllWarehouses()
+        {
+            StorePlacesDTO result;
+            try
+            {
+                var collection = _storePlaceService.GetAll().Where(sp => sp.GetType() == typeof(Warehouse)).ToList();
+
+                result = new StorePlacesDTO
+                {
+                    StorePlaces = collection,
+                    Status = collection.Count > 0 ? CollectionGetStatus.Success : CollectionGetStatus.Empty
+                };
+            }
+            catch
+            {
+                result = new StorePlacesDTO
+                {
+                    StorePlaces = null,
+                    Status = CollectionGetStatus.Failure
+                };
+            }
+
+            return result;
+        }
+        public StorePlacesDTO GetAllSendingPoints()
+        {
+            StorePlacesDTO result;
+            try
+            {
+                var collection = _storePlaceService.GetAll().Where(sp => sp.GetType() == typeof(SendingPoint)).ToList();
+
+                result = new StorePlacesDTO
+                {
+                    StorePlaces = collection,
+                    Status = collection.Count > 0 ? CollectionGetStatus.Success : CollectionGetStatus.Empty
+                };
+            }
+            catch
+            {
+                result = new StorePlacesDTO
+                {
+                    StorePlaces = null,
+                    Status = CollectionGetStatus.Failure
+                };
+            }
+
+            return result;
+        }
+    }
+}
