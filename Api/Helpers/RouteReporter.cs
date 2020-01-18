0ba4bb355db0078da46bfd9a26727cb7f38013c0
Bartek <bartekd97@gmail.com>
2020-01-18 23:21:14 +0100

    Added route reporter

---
+++ Api/Helpers/RouteReporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.Models;
+using System.Diagnostics;
+
+namespace Api.Helpers
+{
+    public class RouteReporter
+    {
+        private Route route;
+
+        public RouteReporter(Route route)
+        {
+            this.route = route;
+        }
+
+        public void GeneratePDF()
+        {
+            string filename = "routereport.pdf";
+
+            // TODO
+
+            Process.Start(filename); // open file in external program
+        }
+    }
+}
--- View/LogisticsRouteViewForm.cs
+++ View/LogisticsRouteViewForm.cs
@@ -11,6 +11,7 @@ using Api.Controllers;
 using Api.DTOs;
 using Model.Models;
 using System.Linq;
+using Api.Helpers;
 
 namespace View
 {
@@ -25,10 +26,40 @@ namespace View
             InitializeComponent();
         }
 
-        private void LogisticsRouteView_Load(object sender, EventArgs e)
+        private void LogisticsRouteViewForm_Shown(object sender, EventArgs e)
         {
-            driverLabel.Text = route.Employee.Surname;
-            vehicleLabel.Text = route.Vehicle.Model;
+            driverLabel.Text = string.Format(
+                    "{0} {1}",
+                    route.Employee.Name,
+                    route.Employee.Surname
+                 );
+            vehicleLabel.Text = string.Format(
+                    "{0} {1} ({2})",
+                    route.Vehicle.Brand,
+                    route.Vehicle.Model,
+                    route.Vehicle.Registration
+                 );
+            dateLabel.Text = string.Format("{0} {1}",
+                    route.CreationDateTime.ToShortDateString(),
+                    route.CreationDateTime.ToShortTimeString()
+                );
+
+            listRoutes.Items.Clear();
+            foreach (var point in route.RoutePoints)
+            {
+                Address addr = point.Parcel.ReceiverData.PersonalAddress;
+                string addressText = string.Format("{0} {1}/{2}, {3}, {4}", addr.Street, addr.HomeNumber, addr.ApartmentNumber, addr.PostCode, addr.City);
+                ListViewItem item = new ListViewItem(new string[] { point.Index.ToString(), addressText });
+                if (point.Parcel.Priority > 0)
+                    item.BackColor = Color.LightGoldenrodYellow;
+                listRoutes.Items.Add(item);
+            }
+        }
+
+        private void pdfButton_Click(object sender, EventArgs e)
+        {
+            RouteReporter reporter = new RouteReporter(route);
+            reporter.GeneratePDF();
         }
     }
 }
